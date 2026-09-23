fd819ac5db0632f8b36f7d9aa5eda7dc6927b7e6
herogeekeduc08-jpg <otaviolz0601@gmail.com>
2026-09-22 21:02:30 -0300

    commitar

--- tcf/src/main.tsx
+++ tcf/src/main.tsx
@@ -3,6 +3,7 @@ import { setupCounter} from './counter.ts'
 import {BrowserRouter} from 'react-router'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   
